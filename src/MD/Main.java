e42a6464d47513c5cd997be20c5cc51961932f47
mdysput <michal.dysput@gmail.com>
2019-02-24 11:03:25 +0100

    BinarySearch first!

--- src/MD/Main.java
+++ src/MD/Main.java
@@ -4,6 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("start");
+        int[] tab= {1, 2, 5, 6, 7, 9};
+        int liczba= binarySearch(tab, 6);
+        if(liczba != -1){
+            System.out.println(liczba);
+        }
     }
+
+    private static int binarySearch(int[] tab, int find){
+        int first=0;
+        int last= tab.length-1;
+
+        while(first<=last){
+             int medium=(first+last)/2;
+
+             if(tab[medium]== find){
+                 return tab[medium];
+             }
+             else if(tab[medium]<find){
+                 first= medium+1;
+             }
+             else
+                 last= medium-1;
+        }
+        return -1;
+    }
+
 }
+
